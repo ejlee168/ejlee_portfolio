1db67c8190bf516b5350e4855fc1c5e3d2463aae
Ethan Lee <ejlee168@gmail.com>
2025-01-27 20:31:06 +1000

    Add light-dark toggle

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function copyEmail() {
   const popup = document.getElementById('copied-popup');
 }
+
+function toggleTheme() {
+  if (document.body.classList.contains('dark-mode')) {
+    document.getElementById('light-dark-toggle').src="assets/sun.svg";
+  } else {
+    document.getElementById('light-dark-toggle').src="assets/moon.svg";
+  }
+  document.body.classList.toggle('dark-mode');
+  
+
+}
