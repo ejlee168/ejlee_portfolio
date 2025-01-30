2115bcc59b7cc98cf18027473c699bf341acbfc4
ejlee168 <118642387+ejlee168@users.noreply.github.com>
2025-01-30 22:17:18 +1000

    Attempt to fix pathing of css and nav bar

--- nav/nav.js
+++ nav/nav.js
@@ -1,13 +1,11 @@
-toggle_sound = new Audio('assets/toggle_sound.mp3');
+toggle_sound = new Audio('/assets/toggle_sound.mp3');
 
 function toggleTheme() {
   toggle_sound.play();
   document.body.classList.toggle('dark-mode');
   if (document.body.classList.contains('dark-mode')) {
-    document.getElementById('light-dark-toggle').src =
-      '/ejlee_website/assets/sun.svg';
+    document.getElementById('light-dark-toggle').src = '/assets/sun.svg';
   } else {
-    document.getElementById('light-dark-toggle').src =
-      '/ejlee_website/assets/moon.svg';
+    document.getElementById('light-dark-toggle').src = '/assets/moon.svg';
   }
 }
