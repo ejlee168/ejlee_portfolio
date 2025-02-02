2fb4ea819bf144548a4642428707f1fb96ae46ff
ejlee168 <118642387+ejlee168@users.noreply.github.com>
2025-02-02 21:39:13 +1000

    Add persistent darkmode with localstorage, has bug with icon needs to fix.

--- nav/nav.js
+++ nav/nav.js
@@ -1,11 +1,23 @@
 toggle_sound = new Audio('/assets/toggle_sound.mp3');
 
+let currentTheme = localStorage.getItem('theme');
+
+if (!currentTheme) {
+  localStorage.setItem('theme', 'light');
+  currentTheme = 'light';
+}
+
 function toggleTheme() {
   toggle_sound.play();
   document.body.classList.toggle('dark-mode');
-  if (document.body.classList.contains('dark-mode')) {
+
+  const isDarkMode = document.body.classList.contains('dark-mode');
+
+  if (isDarkMode) {
+    localStorage.setItem('theme', 'dark');
     document.getElementById('light-dark-toggle').src = '/assets/sun.svg';
   } else {
     document.getElementById('light-dark-toggle').src = '/assets/moon.svg';
+    localStorage.setItem('theme', 'light');
   }
 }
